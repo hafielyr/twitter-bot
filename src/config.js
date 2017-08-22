83428545924d9f9773b8a5b237fbe7aceedc7ace
Hafieludin Yusuf Rizana <Hafieludin_R7234@mitrais.com>
2017-08-22 16:50:31 +0800

    favorited all tweet with hashtag

---
+++ src/config.js
@@ -0,0 +1,7 @@
+// config.js
+module.exports = {
+    consumer_key: 'QE951EKool1pWSjZ4QylzbpDY',
+    consumer_secret: 'w8cUrDPgYQIv83NVXwJKJCjGcb2GO8wjUNBmOHJxLssCPKZ5Jb',
+    access_token_key: '285026167-PW5gHx7BcDqfmXZFVSMP647BUoS4KDJ3ERRFAsbU',
+    access_token_secret: 'BMSLHYiIbdfUkk0fwVajiSmgiJfuOnvrUhWax18Qb5ymz'
+  }
